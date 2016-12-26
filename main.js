4c494abca2f77e4484a505a89ea489a653c25ffa
Gabriela Perez <gabrielaperez1127@gmail.com>
2016-12-26 15:15:23 -0500

    images index main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function calcular() {
+    var hora = document.getElementById("hora");
+    var loro = document.getElementById("loro").value;
+    var img = document.getElementById("images");
+    var varhora=hora.value;
+    var varloro = false;
+    var varsalida = document.getElementById("salida");
+
+    if (loro == "1") {
+        varloro = true;
+    }
+
+   
+    if(varloro){
+      if(varhora>=7 && varhora<=20){
+          varsalida.innerHTML="No";
+      }
+        else{
+            varsalida.innerHTML="Si";
+            img.innerHTML = '<img src="images/lorito.png" class:"img-responsive">';
+        }
+    }
+    else{
+        varsalida.innerHTML="No";
+    }
+}
